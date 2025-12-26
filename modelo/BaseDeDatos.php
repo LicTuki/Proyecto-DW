78c8d57344fecb08000e6fdd1c5f05af1c07b3e8
David <fdvgf525.55@gmail.com>
2025-12-26 10:31:54 -0600

    Cambios_David

--- modelo/BaseDeDatos.php
+++ modelo/BaseDeDatos.php
@@ -1,5 +1,19 @@
 <?php
-class BaseDeDatos{
-  
+class BaseDeDatos
+{
+  private function conectarDB() {}
+
+  public function validarUsuario($nombreUsuario, $claveUsuario) {}
+
+  // Opciones de lo servidores
+  public function agregarServidor($nombreServidor, $ipServidor) {}
+
+  public function editarServidor($idServidor, $nombreServidor, $ipServidor) {}
+
+  public function eliminarServidor($idServidor) {}
+
+  // Opciones de los estados de los servidores 
+  public function servidorActivo($ipServidor, $tokenServidor) {}
+
+  public function servidorApagar($ipServidor, $tokenServidor) {}
 }
-?>
